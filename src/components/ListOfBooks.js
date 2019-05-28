4f9b3480382806ecfcdafa48512ccc3bf64da7f7
Rezende <pedro.rezende@vistajet.com>
2019-05-28 13:54:51 -0300

    changed the selected tag.

--- src/components/ListOfBooks.js
+++ src/components/ListOfBooks.js
@@ -18,12 +18,12 @@ class ListOfBooks extends React.Component {
 				<div className="book-cover" style={{ width: 128, height: 193, 
 					backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
 					<div className="book-shelf-changer">
-					<select value={book.title} onChange={e => this.props.changeType(book, e.target.value)}>
-					<option value="move">Move to...</option>
-					<option value="currentlyReading">Currently Reading</option>
-					<option value="wantToRead">Want to Read</option>
-					<option value="read">Read</option>
-					<option value="none">None</option>
+					<select value={book.shelf} onChange={e => this.props.changeType(book, e.target.value)}>
+						<option value="move">Move to...</option>
+						<option value="currentlyReading" selected>Currently Reading</option>
+						<option value="wantToRead">Want to Read</option>
+						<option value="read">Read</option>
+						<option value="none">None</option>
 					</select>
 					</div>
 					</div>
